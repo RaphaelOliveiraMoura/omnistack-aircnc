2d12293bb527de09548c9cbda764cf57ac438a9f
RaphaelOliveiraMoura <raphaeldeoliveiramoura@gmail.com>
2019-10-05 21:02:34 -0300

    static files routes back end

--- back/src/server.js
+++ back/src/server.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const path = require('path');
 const express = require('express');
 const routes = require('./routes');
 const mongoose = require('mongoose');
@@ -12,6 +13,7 @@ mongoose.connect(process.env.MONGO_DB_URL, {
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 app.listen(3333, () => console.log('Application running ...'));
