0790239911d85b305e5ef5d1b0d31af5e8f9021f
RaphaelOliveiraMoura <raphaeldeoliveiramoura@gmail.com>
2019-10-06 02:31:55 -0300

    mobile finished

---
+++ mobile/src/pages/List.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from 'react';
+
+import {
+  AsyncStorage,
+  Image,
+  StyleSheet,
+  ScrollView,
+  SafeAreaView
+} from 'react-native';
+
+import logo from '../../assets/logo.png';
+
+import SpotList from '../components/SpotList';
+
+export default function List() {
+  const [techs, setTechs] = useState([]);
+
+  useEffect(() => {
+    AsyncStorage.getItem('techs').then(storagedTechs => {
+      const techsArray = storagedTechs.split(',').map(tech => tech.trim());
+      setTechs(techsArray);
+    });
+  }, []);
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Image style={styles.logo} source={logo} />
+      <ScrollView>
+        {techs.map((tech, index) => (
+          <SpotList key={index} tech={tech} />
+        ))}
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  logo: {
+    height: 32,
+    resizeMode: 'contain',
+    alignSelf: 'center',
+    marginTop: 40
+  }
+});
